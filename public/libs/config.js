7cdfc79fd736e97290ae014bb965aff10c9e23b0
许婷 <1481732939@qq.com>
2017-04-18 09:32:34 +0800

    完成了登录和退出登录功能

---
+++ public/libs/config.js
@@ -0,0 +1,22 @@
+requirejs.config({
+	//直接改变基目录
+	baseUrl:'./public',
+	//直接指定它的网址或路径
+	paths:{
+		'jquery':'assets/jquery/jquery.min',
+		'cookie':'assets/jquery-cookie/jquery.cookie',
+		'bootstrap':'assets/bootstrap/js/bootstrap.min',
+		'echarts':'assets/echarts/echarts.min',
+		'template':'assets/artTemplate/template-native'
+	},
+	//shim属性，配置不兼容的模块。每个模块要定义（1）exports值（输出的变量名），表明这个模块外部调用时的名称；（2）deps数组，表明该模块的依赖性。
+	shim:{
+		'bootstrap':{
+			exports:'bootstrap',
+			deps:['jquery']
+		}
+	}
+});
+
+//退出登录
+require(['src/common']);
---
+++ public/src/common.js
@@ -0,0 +1,46 @@
+/*
+	NProgress.start();
+
+	NProgress.done();
+
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});*/
+
+//检测用户是否登录，如果没有登录跳转到登录界面
+//如果用户登录了，会在服务器端存储一个session，浏览器会设置一个名字叫PHPSESSID的cookie
+
+//只要检测PHPSESSID是否存在就能判断用户是否登录过
+//通过document.cookie可以读取本地存的cookie
+//location对象，可以获得浏览器的地址信息
+
+define(['jquery','cookie'],function($){
+	//检测用户是否登录
+	if(document.cookie.indexOf('PHPSESSID') == -1 && location.pathname != '/login'){
+		//PHPSESSID不存在，说明未登录过，若不是登录界面，则跳转至登录界面
+		location.href = '/login';
+	}
+	
+	//获取用户登录信息，储存在cookie中
+	//JSON.parse()解析一个JSON字符串，构造由字符串描述的JavaScript值或对象
+	//未登录前不存在，为undefined
+	var loginInfo = $.cookie('loginInfo') && JSON.parse($.cookie('loginInfo'));
+	
+	//退出登录：点击"退出"按钮，发送ajax请求
+	$('#logout').click(function(){
+		$.ajax({
+			url:'/api/logout',
+			type:'post',
+			success:function(info){
+				if (info.code == 200) {
+					location.href = '/login';
+					/*var date=new Date();
+					//将date设置为过去的时间
+					date.setTime(date.getTime()-10000);
+					//将cookie删除
+					document.cookie="expire="+date.toGMTString();*/
+				}
+			}
+		});
+	});
+});
